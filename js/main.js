0dcaf5d9d586d5f6a0f9f21b8695752792000bea
gabrielfwtz@gmail.com <gabrielfwtz@gmail.com>
2024-05-12 11:43:09 -0300

    Added Loader style and js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function () {
+    // Ocultar o overlay e mostrar a seção com ID main
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('main').style.display = 'block';
+});
